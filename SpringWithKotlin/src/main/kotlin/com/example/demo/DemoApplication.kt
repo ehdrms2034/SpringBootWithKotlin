8f85c6d49ecdf74421252dfcba950765234eea78
Kim Dong Geun <ehdrms2034@naver.com>
2019-11-06 22:13:47 +0900

    chapter2 autowired

--- SpringWithKotlin/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ SpringWithKotlin/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,8 +1,10 @@
 package com.example.demo
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
@@ -12,9 +14,12 @@ class DemoApplication
 
 @Controller
 class FirstConstroller{
-	@RequestMapping(value= ["/","/user"], method = arrayOf(RequestMethod.GET))
+	@Autowired
+	lateinit var service : ServiceInterface
+
+	@RequestMapping(value= ["/{name}","/user/{name}"], method = arrayOf(RequestMethod.GET))
 	@ResponseBody
-	fun hello() = "hello World"
+	fun hello(@PathVariable name: String) = service.getHello(name)
 }
 
 fun main(args: Array<String>) {
